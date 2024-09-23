112bccaafeefa089bd8e386d3310b28ab72b9c76
bbombassei <47109867+bbombassei@users.noreply.github.com>
2024-09-23 18:00:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,120 +3,120 @@ $(document).ready(function() {
         {
             question: "Name something adults do at Halloween parties.",
             answers: [
-                { answer: "Drink alcohol", points: 40 },
-                { answer: "Wear sexy costumes", points: 30 },
-                { answer: "Dance", points: 20 },
-                { answer: "Carve pumpkins", points: 10 },
-                { answer: "Tell scary stories", points: 5 },
-                { answer: "Play drinking games", points: 5 }
+                { answer: "Drink alcohol", points: 40, variations: ["drinking", "booze", "party"] },
+                { answer: "Wear sexy costumes", points: 30, variations: ["sexy outfit", "costumes"] },
+                { answer: "Dance", points: 20, variations: ["dancing"] },
+                { answer: "Carve pumpkins", points: 10, variations: ["pumpkin carving"] },
+                { answer: "Tell scary stories", points: 5, variations: ["storytelling", "scary stories"] },
+                { answer: "Play drinking games", points: 5, variations: ["drinking games"] }
             ],
             multiplier: 1
         },
         {
             question: "Name a popular Halloween costume for women.",
             answers: [
-                { answer: "Sexy witch", points: 50 },
-                { answer: "Catwoman", points: 40 },
-                { answer: "Vampire", points: 30 },
-                { answer: "Nurse", points: 20 },
-                { answer: "Cheerleader", points: 10 },
-                { answer: "Zombie", points: 5 }
+                { answer: "Sexy witch", points: 50, variations: ["witch", "sexy witch costume"] },
+                { answer: "Catwoman", points: 40, variations: ["cat woman"] },
+                { answer: "Vampire", points: 30, variations: ["vamp"] },
+                { answer: "Nurse", points: 20, variations: ["sexy nurse"] },
+                { answer: "Cheerleader", points: 10, variations: ["cheerleader costume"] },
+                { answer: "Zombie", points: 5, variations: ["zombie costume"] }
             ],
             multiplier: 1
         },
         {
             question: "Name a classic Halloween horror movie.",
             answers: [
-                { answer: "Halloween", points: 50 },
-                { answer: "Nightmare on Elm Street", points: 40 },
-                { answer: "Friday the 13th", points: 30 },
-                { answer: "The Exorcist", points: 20 },
-                { answer: "Scream", points: 10 },
-                { answer: "The Shining", points: 5 }
+                { answer: "Halloween", points: 50, variations: [] },
+                { answer: "Nightmare on Elm Street", points: 40, variations: ["nightmare on elm"] },
+                { answer: "Friday the 13th", points: 30, variations: [] },
+                { answer: "The Exorcist", points: 20, variations: [] },
+                { answer: "Scream", points: 10, variations: [] },
+                { answer: "The Shining", points: 5, variations: [] }
             ],
             multiplier: 1
         },
         {
             question: "Name a Halloween decoration you see everywhere.",
             answers: [
-                { answer: "Pumpkins", points: 50 },
-                { answer: "Skeletons", points: 40 },
-                { answer: "Spiders/Webs", points: 30 },
-                { answer: "Gravestones", points: 20 },
-                { answer: "Ghosts", points: 10 },
-                { answer: "Bats", points: 5 }
+                { answer: "Pumpkins", points: 50, variations: [] },
+                { answer: "Skeletons", points: 40, variations: [] },
+                { answer: "Spiders/Webs", points: 30, variations: ["spider webs"] },
+                { answer: "Gravestones", points: 20, variations: ["tombstones"] },
+                { answer: "Ghosts", points: 10, variations: [] },
+                { answer: "Bats", points: 5, variations: [] }
             ],
             multiplier: 2
         },
         {
             question: "Name a Halloween monster people dress up as.",
             answers: [
-                { answer: "Vampire", points: 50 },
-                { answer: "Witch", points: 40 },
-                { answer: "Werewolf", points: 30 },
-                { answer: "Frankenstein", points: 20 },
-                { answer: "Zombie", points: 10 },
-                { answer: "Mummy", points: 5 }
+                { answer: "Vampire", points: 50, variations: [] },
+                { answer: "Witch", points: 40, variations: [] },
+                { answer: "Werewolf", points: 30, variations: [] },
+                { answer: "Frankenstein", points: 20, variations: [] },
+                { answer: "Zombie", points: 10, variations: [] },
+                { answer: "Mummy", points: 5, variations: [] }
             ],
             multiplier: 2
         },
         {
             question: "Name a spooky sound you hear on Halloween.",
             answers: [
-                { answer: "Screams", points: 50 },
-                { answer: "Wolf howls", points: 40 },
-                { answer: "Creaking doors", points: 30 },
-                { answer: "Thunder", points: 20 },
-                { answer: "Chains rattling", points: 10 },
-                { answer: "Evil laughter", points: 5 }
+                { answer: "Screams", points: 50, variations: [] },
+                { answer: "Wolf howls", points: 40, variations: ["wolf howl"] },
+                { answer: "Creaking doors", points: 30, variations: [] },
+                { answer: "Thunder", points: 20, variations: [] },
+                { answer: "Chains rattling", points: 10, variations: [] },
+                { answer: "Evil laughter", points: 5, variations: [] }
             ],
             multiplier: 2
         },
         {
             question: "Name something people fear about Halloween night.",
             answers: [
-                { answer: "Ghosts", points: 50 },
-                { answer: "Darkness", points: 40 },
-                { answer: "Witches", points: 30 },
-                { answer: "Haunted houses", points: 20 },
-                { answer: "Vandalism", points: 10 },
-                { answer: "Getting pranked", points: 5 }
+                { answer: "Ghosts", points: 50, variations: [] },
+                { answer: "Darkness", points: 40, variations: [] },
+                { answer: "Witches", points: 30, variations: [] },
+                { answer: "Haunted houses", points: 20, variations: ["haunted house"] },
+                { answer: "Vandalism", points: 10, variations: [] },
+                { answer: "Getting pranked", points: 5, variations: [] }
             ],
             multiplier: 3
         },
         {
             question: "Name a reason adults dislike trick-or-treating.",
             answers: [
-                { answer: "Too many kids", points: 50 },
-                { answer: "Too late at night", points: 40 },
-                { answer: "Don't like candy", points: 30 },
-                { answer: "Costumes are expensive", points: 20 },
-                { answer: "Answering the door repeatedly", points: 10 },
-                { answer: "Annoying parents", points: 5 }
+                { answer: "Too many kids", points: 50, variations: [] },
+                { answer: "Too late at night", points: 40, variations: [] },
+                { answer: "Don't like candy", points: 30, variations: [] },
+                { answer: "Costumes are expensive", points: 20, variations: [] },
+                { answer: "Answering the door repeatedly", points: 10, variations: [] },
+                { answer: "Annoying parents", points: 5, variations: [] }
             ],
             multiplier: 3
         },
         {
             question: "Name a classic horror movie villain.",
             answers: [
-                { answer: "Freddy Krueger", points: 50 },
-                { answer: "Michael Myers", points: 40 },
-                { answer: "Jason Voorhees", points: 30 },
-                { answer: "Chucky", points: 20 },
-                { answer: "Ghostface", points: 10 },
-                { answer: "Leatherface", points: 5 }
+                { answer: "Freddy Krueger", points: 50, variations: [] },
+                { answer: "Michael Myers", points: 40, variations: [] },
+                { answer: "Jason Voorhees", points: 30, variations: [] },
+                { answer: "Chucky", points: 20, variations: [] },
+                { answer: "Ghostface", points: 10, variations: [] },
+                { answer: "Leatherface", points: 5, variations: [] }
             ],
             multiplier: 3
         },
         {
             question: "Name a Halloween prank people play.",
             answers: [
-                { answer: "Egging houses", points: 50 },
-                { answer: "Toilet papering trees", points: 40 },
-                { answer: "Ding-dong ditch", points: 30 },
-                { answer: "Jump scares", points: 20 },
-                { answer: "Fake spiders", points: 10 },
-                { answer: "Switching candy with something gross", points: 5 }
+                { answer: "Egging houses", points: 50, variations: [] },
+                { answer: "Toilet papering trees", points: 40, variations: [] },
+                { answer: "Ding-dong ditch", points: 30, variations: [] },
+                { answer: "Jump scares", points: 20, variations: [] },
+                { answer: "Fake spiders", points: 10, variations: [] },
+                { answer: "Switching candy with something gross", points: 5, variations: [] }
             ],
             multiplier: 3
         }
@@ -126,24 +126,17 @@ $(document).ready(function() {
     let boysScore = 0;
     let girlsScore = 0;
 
-    function updateRound() {
-        $('#question').text(rounds[currentRound].question);
-        $('#round-title').text('Round ' + (currentRound + 1));
-        $('#multiplier').text('Points: ' + rounds[currentRound].multiplier + 'x');
-        $('.answer-text').text('-');
-        $('.points').text('');
-        $('#user-answer').val('');
-    }
-
-    function submitAnswer() {
-        const userAnswer = $('#user-answer').val().toLowerCase();
+    function checkAnswer(userAnswer) {
         let correctAnswer = false;
 
         $('.answer').each(function(index) {
-            const answerText = rounds[currentRound].answers[index].answer.toLowerCase();
-            if (userAnswer === answerText && $(this).find('.answer-text').text() === '-') {
-                const points = rounds[currentRound].answers[index].points * rounds[currentRound].multiplier;
-                $(this).find('.answer-text').text(rounds[currentRound].answers[index].answer);
+            const answerObj = rounds[currentRound].answers[index];
+            const correctText = answerObj.answer.toLowerCase();
+            const variations = answerObj.variations.map(v => v.toLowerCase());
+
+            if (userAnswer === correctText || variations.includes(userAnswer)) {
+                const points = answerObj.points * rounds[currentRound].multiplier;
+                $(this).find('.answer-text').text(answerObj.answer);
                 $(this).find('.points').text(points);
                 
                 // Alternate scoring between teams
@@ -159,7 +152,13 @@ $(document).ready(function() {
             }
         });
 
-        if (!correctAnswer) {
+        return correctAnswer;
+    }
+
+    function submitAnswer() {
+        const userAnswer = $('#user-answer').val().toLowerCase();
+
+        if (!checkAnswer(userAnswer)) {
             alert('Wrong answer! Try again.');
         }
 
@@ -174,6 +173,23 @@ $(document).ready(function() {
             updateRound();
         } else {
             alert('Fast Money Round Coming Soon!');
+        }
+    });
+
+    function updateRound() {
+        $('#question').text(rounds[currentRound].question);
+        $('#round-title').text('Round ' + (currentRound + 1));
+        $('#multiplier').text('Points: ' + rounds[currentRound].multiplier + 'x');
+        $('.answer-text').text('-');
+        $('.points').text('');
+        $('#user-answer').val('');
+    }
+
+    // Handle Enter key for submitting answers
+    $(document).keydown(function(event) {
+        if (event.key === 'Enter') {
+            event.preventDefault(); // Prevent form submission if wrapped in a form
+            submitAnswer(); // Call the function to submit the answer
         }
     });
 
