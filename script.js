e2b9449bc53e7b8012c1ff43a5faed0bbf0f7d47
bbombassei <47109867+bbombassei@users.noreply.github.com>
2024-09-23 17:49:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -22,17 +22,101 @@ $(document).ready(function() {
                 { answer: "Cheerleader", points: 10 },
                 { answer: "Zombie", points: 5 }
             ],
+            multiplier: 1
+        },
+        {
+            question: "Name a classic Halloween horror movie.",
+            answers: [
+                { answer: "Halloween", points: 50 },
+                { answer: "Nightmare on Elm Street", points: 40 },
+                { answer: "Friday the 13th", points: 30 },
+                { answer: "The Exorcist", points: 20 },
+                { answer: "Scream", points: 10 },
+                { answer: "The Shining", points: 5 }
+            ],
+            multiplier: 1
+        },
+        {
+            question: "Name a Halloween decoration you see everywhere.",
+            answers: [
+                { answer: "Pumpkins", points: 50 },
+                { answer: "Skeletons", points: 40 },
+                { answer: "Spiders/Webs", points: 30 },
+                { answer: "Gravestones", points: 20 },
+                { answer: "Ghosts", points: 10 },
+                { answer: "Bats", points: 5 }
+            ],
             multiplier: 2
         },
         {
-            question: "Name a Halloween-themed drink.",
+            question: "Name a Halloween monster people dress up as.",
             answers: [
-                { answer: "Bloody Mary", points: 50 },
-                { answer: "Pumpkin spice cocktail", points: 40 },
-                { answer: "Witches' brew", points: 30 },
-                { answer: "Vampire's kiss", points: 20 },
-                { answer: "Jello shots", points: 10 },
-                { answer: "Spider venom shots", points: 5 }
+                { answer: "Vampire", points: 50 },
+                { answer: "Witch", points: 40 },
+                { answer: "Werewolf", points: 30 },
+                { answer: "Frankenstein", points: 20 },
+                { answer: "Zombie", points: 10 },
+                { answer: "Mummy", points: 5 }
+            ],
+            multiplier: 2
+        },
+        {
+            question: "Name a spooky sound you hear on Halloween.",
+            answers: [
+                { answer: "Screams", points: 50 },
+                { answer: "Wolf howls", points: 40 },
+                { answer: "Creaking doors", points: 30 },
+                { answer: "Thunder", points: 20 },
+                { answer: "Chains rattling", points: 10 },
+                { answer: "Evil laughter", points: 5 }
+            ],
+            multiplier: 2
+        },
+        {
+            question: "Name something people fear about Halloween night.",
+            answers: [
+                { answer: "Ghosts", points: 50 },
+                { answer: "Darkness", points: 40 },
+                { answer: "Witches", points: 30 },
+                { answer: "Haunted houses", points: 20 },
+                { answer: "Vandalism", points: 10 },
+                { answer: "Getting pranked", points: 5 }
+            ],
+            multiplier: 3
+        },
+        {
+            question: "Name a reason adults dislike trick-or-treating.",
+            answers: [
+                { answer: "Too many kids", points: 50 },
+                { answer: "Too late at night", points: 40 },
+                { answer: "Don't like candy", points: 30 },
+                { answer: "Costumes are expensive", points: 20 },
+                { answer: "Answering the door repeatedly", points: 10 },
+                { answer: "Annoying parents", points: 5 }
+            ],
+            multiplier: 3
+        },
+        {
+            question: "Name a classic horror movie villain.",
+            answers: [
+                { answer: "Freddy Krueger", points: 50 },
+                { answer: "Michael Myers", points: 40 },
+                { answer: "Jason Voorhees", points: 30 },
+                { answer: "Chucky", points: 20 },
+                { answer: "Ghostface", points: 10 },
+                { answer: "Leatherface", points: 5 }
+            ],
+            multiplier: 3
+        },
+        {
+            question: "Name a Halloween prank people play.",
+            answers: [
+                { answer: "Egging houses", points: 50 },
+                { answer: "Toilet papering trees", points: 40 },
+                { answer: "Ding-dong ditch", points: 30 },
+                { answer: "Jump scares", points: 20 },
+                { answer: "Fake spiders", points: 10 },
+                { answer: "Switching candy with something gross", points: 5 }
             ],
             multiplier: 3
         }
@@ -73,4 +157,26 @@ $(document).ready(function() {
 
                 correctAnswer = true;
             }
-       
+        });
+
+        if (!correctAnswer) {
+            alert('Wrong answer! Try again.');
+        }
+
+        $('#user-answer').val('');
+    }
+
+    $('#submit-answer').click(submitAnswer);
+
+    $('#next-round').click(function() {
+        if (currentRound < rounds.length - 1) {
+            currentRound++;
+            updateRound();
+        } else {
+            alert('Fast Money Round Coming Soon!');
+        }
+    });
+
+    // Initialize first round
+    updateRound();
+});
